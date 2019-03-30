7147df08d7f7f736f97402311c37c49fe767105b
vagrant <vagrant@vagrant.vm>
2019-03-30 00:31:17 +0000

    updated code and readme

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
-The reporting answer the 3 following questions:
+
+# Log summary  
+This project is a part of the Udacity's Full Stack Web Developer Nanodegree.
+The task is to create a reporting tool that prints out reports (in plain text) based on the data in the database. This reporting tool is a Python program using the psycopg2 module to connect to the database.
+
+
+The reporting answer the 3 following questions from :
 
 What are the most popular three articles of all time?
 Who are the most popular article authors of all time?
 On which days did more than 1% of requests lead to errors?
 
+
+## Prerequisites
+'''Python 3''' (https://www.python.org/) 
+'''Vagrant''' (https://www.vagrantup.com/downloads.html)
+'''VirtualBox''' (https://www.virtualbox.org/wiki/Downloads)
+'''Vagrantfile''' (https://github.com/udacity/fullstack-nanodegree-vm)
+
+Starting the Ubuntu Linux installation with '''vagrant up'''.
+Logging into the Linux VM with '''vagrant ssh'''. Load the data into the PostgreSQL database. Use the command '''psql -d news -f newsdata.sql'''.
+
+
+
+There are three tablesin the database:
+The '''authors''' table includes information about the authors of articles.
+The '''articles''' table includes the articles themselves.
+The '''log table''' includes one entry for each time a user has accessed the site.
+
+
+#Running the program 
 The sql query can be run as 
-python analysis.py 
+python3 analysis.py 
--- analysis.py
+++ analysis.py
@@ -1,54 +1,59 @@
+#!/usr/bin/env python3
+
+
 import psycopg2
 
 
-def perform_query(cur, sql, explanation = 'views'):
-	 
-        cur.execute(sql)
-	row = [item for item in cur.fetchall()]
-        for r in row:
-	    print "{}--{} {}".format(r[0],r[1], explanation);
-	print ("**************************")
+def perform_query(cur, sql, explanation='views'):
+    cur.execute(sql)
+    row = [item for item in cur.fetchall()]
+    for r in row:
+        print ("{}--{} {}".format(r[0], r[1], explanation))
+    print ("**************************")
+
 
 def main():
     conn = psycopg2.connect("dbname=news")
     cur = conn.cursor()
 
-    # Q1
-    print ("1. What are the most popular three articles of all time?")    
-    sql_query =  """
+# Q1
+    print ("1. What are the most popular three articles of all time?")
+    sql_query = """
                  select title, count(*) as view_count
                  from log join articles
-                 on articles.slug = SPLIT_PART(log.path,'/',3) 
+                 on articles.slug = SPLIT_PART(log.path,'/',3)
                  group by title
                  order by view_count desc
                  limit 3;
                 """
-    perform_query(cur, sql_query) 
-	
-    # Q2
-    print ("2. Who are the most popular article authors of all time? ")    
-    sql_query =  """
+    perform_query(cur, sql_query)
+# Q2
+    print ("2. Who are the most popular article authors of all time? ")
+    sql_query = """
                   select name, count(*) as view_count
-                  from (authors join articles 
-                  on authors.id = articles.author) 
-		  join log  
-                  on articles.slug = SPLIT_PART(log.path,'/',3) 
+                  from (authors join articles
+                  on authors.id = articles.author)
+                  join log
+                  on articles.slug = SPLIT_PART(log.path,'/',3)
                   group by name
                   order by view_count desc;
                 """
-    perform_query(cur, sql_query) 
-    #Q3
+    perform_query(cur, sql_query)
+# Q3
     print ("3. On which days did more than 1% of requests lead to errors?")
-    sql_query =  """
+    sql_query = """
        select to_char(date,'MON DD YYYY'), err/total*100 as ratio
        from (select cast(time as date) as date,
                     count(*) as total,
-                    cast(sum(cast((status not like '200 OK') as int)) as float) as err
+                    cast(sum(cast((status not like '200 OK') as int)) as float)
+                    as err
                     from log
                     group by date) as errors
        where err/total > 0.01;
 
     """
-    perform_query(cur, sql_query, '% errors') 
+    perform_query(cur, sql_query, '% errors')
+
+
 if __name__ == "__main__":
-	main()
+    main()
